4533bd5039737a2fbfb25e80451d6b35b59a6a4b
tracer-ics2015 <tracer@njuics.org>
2015-10-23 06:29:59 -0700

    >  run 141220000 auser Linux (none) 3.8.4-102.fc17.x86_64 #1 SMP Sun Mar 24 13:09:09 UTC 2013 x86_64 x86_64 x86_64 GNU/Linux  06:29:59 up  8:07,  2 users,  load average: 0.02, 0.07, 0.06 c32eef12aca5e2b273aa6715c2b2d12cb281d73f

--- nemu/src/cpu/exec/arith/add.h
+++ nemu/src/cpu/exec/arith/add.h
@@ -5,10 +5,12 @@ make_helper(add_i2rm_b);
 make_helper(add_si2rm_b);
 make_helper(add_r2rm_b);
 make_helper(add_rm2r_b);
+make_helper(add_i2a_b);
 
 make_helper(add_i2rm_v);
 make_helper(add_si2rm_v);
 make_helper(add_r2rm_v);
 make_helper(add_rm2r_v);
+make_helper(add_i2a_v);
 
 #endif
