f31eca08f8ecd298d69bfe78fb0553eac1a19520
tracer-ics2015 <tracer@njuics.org>
2015-10-23 23:45:22 -0700

    >  run 141220000 auser Linux (none) 3.8.4-102.fc17.x86_64 #1 SMP Sun Mar 24 13:09:09 UTC 2013 x86_64 x86_64 x86_64 GNU/Linux  23:45:22 up 12:44,  2 users,  load average: 0.00, 0.01, 0.05 c1d65f20b98ced1cadeba79f56875e19de430831

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -24,9 +24,16 @@ typedef struct {
 
 	/* Do NOT change the order of the GPRs' definitions. */
 
-		struct {
-			uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-		};
+        struct{
+            union{uint32_t eax; uint16_t ax;  uint8_t al, ah;};
+            union{uint32_t ecx; uint16_t cx;  uint8_t cl, ch;};
+            union{uint32_t edx; uint16_t dx;  uint8_t dl, dh;};
+            union{uint32_t ebx; uint16_t bx;  uint8_t bl, bh;};
+            union{uint32_t esp; uint16_t sp;};
+            union{uint32_t ebp; uint16_t bp;};
+            union{uint32_t esi; uint16_t si;};
+            union{uint32_t edi; uint16_t di;};
+            };
 	};
 	struct {
 		uint32_t CF:1;
--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -39,6 +39,33 @@ make_instr_helper(si)
 
 #undef instr
 
+#define instr jbe
+
+static void do_execute() {
+	if(cpu.ZF == 1 || cpu.CF == 1) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#define instr jc
+
+static void do_execute() {
+	if(cpu.CF == 1) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+
 #define instr je
 
 static void do_execute() {
@@ -52,10 +79,36 @@ make_instr_helper(si)
 
 #undef instr
 
-#define instr jbe
+#define instr jz
 
 static void do_execute() {
-	if(cpu.ZF == 1 || cpu.CF == 1) {
+	if(cpu.ZF == 1) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#define instr jg
+
+static void do_execute() {
+	if(cpu.ZF == 0 && cpu.SF == cpu.OF) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#define instr jge
+
+static void do_execute() {
+	if(cpu.SF == cpu.OF) {
 		cpu.eip += op_src->val;
 	}
 	print_asm_template1();
@@ -92,10 +145,10 @@ make_instr_helper(si)
 
 #undef instr
 
-#define instr jg
+#define instr jna
 
 static void do_execute() {
-	if(cpu.ZF == 0 && cpu.SF == cpu.OF) {
+	if(cpu.ZF == 1 || cpu.CF == 1) {
 		cpu.eip += op_src->val;
 	}
 	print_asm_template1();
@@ -105,10 +158,49 @@ make_instr_helper(si)
 
 #undef instr
 
-#define instr jge
+#define instr jnae
 
 static void do_execute() {
-	if(cpu.SF == cpu.OF) {
+	if(cpu.CF == 1) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#define instr jnb
+
+static void do_execute() {
+	if(cpu.CF == 0) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#define instr jnbe
+
+static void do_execute() {
+	if(cpu.ZF == 0 && cpu.CF == 0) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#define instr jnc
+
+static void do_execute() {
+	if(cpu.CF == 0) {
 		cpu.eip += op_src->val;
 	}
 	print_asm_template1();
@@ -131,6 +223,84 @@ make_instr_helper(si)
 
 #undef instr
 
+#define instr jng
+
+static void do_execute() {
+	if(cpu.ZF == 1||cpu.SF != cpu.OF) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#define instr jnge
+
+static void do_execute() {
+	if(cpu.SF != cpu.OF) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#define instr jnl
+
+static void do_execute() {
+	if(cpu.SF == cpu.OF) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#define instr jnle
+
+static void do_execute() {
+	if(cpu.ZF == 0 && cpu.SF == cpu.OF) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#define instr jno
+
+static void do_execute() {
+	if(cpu.OF == 0) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#define instr jnp
+
+static void do_execute() {
+	if(cpu.PF == 0) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
 #define instr jns
 
 static void do_execute() {
@@ -143,5 +313,106 @@ static void do_execute() {
 make_instr_helper(si)
 
 #undef instr
+
+#define instr jnz
+
+static void do_execute() {
+	if(cpu.ZF == 0) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#define instr jo
+
+static void do_execute() {
+	if(cpu.OF == 1) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#define instr jp
+
+static void do_execute() {
+	if(cpu.PF == 1) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#define instr jpe
+
+static void do_execute() {
+	if(cpu.PF == 1) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#define instr jpo
+
+static void do_execute() {
+	if(cpu.PF == 0) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#define instr js
+
+static void do_execute() {
+	if(cpu.SF == 1) {
+		cpu.eip += op_src->val;
+	}
+	print_asm_template1();
+}
+
+make_instr_helper(si)
+
+#undef instr
+
+#if DATA_BYTE == 1
+int concat(jcxz_si_, SUFFIX)(swaddr_t eip){
+    int len = concat(decode_i_, SUFFIX)(eip + 1);
+    len += 1;
+    if (reg_w(R_ECX) == 0){
+        DATA_TYPE_S val0 = op_src -> val;
+        int val = val0;
+        return len + val;
+    }
+    return len;
+}
+int concat(jecxz_si_, SUFFIX)(swaddr_t eip){
+    int len = concat(decode_i_, SUFFIX)(eip + 1);
+    len += 1;
+    if (reg_l(R_ECX) == 0){
+        DATA_TYPE_S val0 = op_src -> val;
+        int val = val0;
+        return len + val;
+    }
+    return len;
+}
+#endif
 
 #include "cpu/exec/template-end.h"
